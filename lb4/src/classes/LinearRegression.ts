6295d58299648fed819eddde85be788eb5963f96
FenrisuIven <120093556+FenrisuIven@users.noreply.github.com>
2025-10-02 19:43:43 +0300

    LinearRegression class, fix coefficients calculation

---
+++ lb4/src/classes/LinearRegression.ts
@@ -0,0 +1,96 @@
+import {type MathNode, parse} from "mathjs"
+
+export class LinearRegression {
+    private _equation: MathNode;
+    private _N: number;
+    private _slope: number;  // k
+    private _offset: number; // b
+
+    private _calculatedPoints: Array<[number, number]>;
+    private _noisedPoints: Array<[number, number]>;
+
+    public get equationString(){
+        return this._equation.toString();
+    }
+    public set equation(newEq: string) {
+        this._equation = parse(newEq);
+    }
+    public get NoisedPoints(){
+        return this._noisedPoints;
+    }
+    public get desmosCalculatedPoints(){
+        return this._calculatedPoints.map(point => `(${point[0]}, ${point[1]})`).join(', ');
+    }
+    public get desmosNoisedPoints(){
+        return this._noisedPoints.map(point => `(${point[0]}, ${point[1]})`).join(', ');
+    }
+
+    constructor({equation, N, slope, offset}: {
+        equation: string,
+        N?: number,
+        slope?: number
+        offset?: number
+    }) {
+        this._equation = parse(equation);
+        this._N = N || 19;
+        this._slope = slope || -2;
+        this._offset = offset || 4;
+        this._calculatedPoints = [];
+        this._noisedPoints = [];
+
+        this.CalculatePoints();
+    }
+
+    public GetValueFor(x: number): number {
+        return this._equation.evaluate({ x, k: this._slope, b: this._offset });
+    }
+
+    public CalculatePoints(args? : { start: number; end: number }) {
+        const { start, end } = args || {};
+        const interval: number[] = [];
+        if (start && end) {
+            if (start < end) {
+                interval.push(start, end);
+            }
+        } else {
+            interval.push(this._N - 10, this._N + 10);
+        }
+
+        const points: Array<[number, number]> = [];
+        for (let x = interval[0]; x < interval[1]; x++) {
+            const y = this.GetValueFor(x);
+            points.push([x, y]);
+        }
+
+        this._calculatedPoints = points;
+        return points;
+    }
+
+    public CalculateNoisedPoints(args?: {noiseStd: number}){
+        const { noiseStd } = args || {};
+        const noisedPoints: Array<[number, number]> = [];
+        const std = noiseStd || (this._N / 5);
+        for (let i = 0; i < this._calculatedPoints.length; i++) {
+            const [x, y] = this._calculatedPoints[i];
+            const noise = (Math.random() - 0.5) * std;
+            noisedPoints.push([x, y + noise]);
+        }
+
+        this._noisedPoints = noisedPoints;
+        return noisedPoints;
+    }
+
+    public CalculateLinearRegressionCoefficients(useNoised: boolean = true) {
+        const points = useNoised ? this._noisedPoints : this._calculatedPoints;
+        const n = points.length;
+        const sumX = points.reduce((acc, curr)=>acc + curr[0], 0);
+        const sumY = points.reduce((acc, curr)=>acc + curr[1], 0);
+        const sumXY = points.reduce((acc, curr, i) => acc + curr[0] * points[i][1], 0);
+        const sumX2 = points.reduce((acc, curr) => acc + curr[0] * curr[0], 0);
+
+        const k = (n*sumXY - sumX*sumY) / (n*sumX2 - sumX*sumX);
+        const b = (sumY - k*sumX) / n;
+
+        return { k, b };
+    }
+}
--- lb4/src/main.tsx
+++ lb4/src/main.tsx
@@ -1,48 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import {LinearRegression} from "./classes/LinearRegression.ts";
 
 const N = 19;
 const k = -2;
 const b = 4;
 
-const y = (x: number) => k * x + b;
+const LR = new LinearRegression({
+  equation: 'k * x + b',
+  N,
+  slope: k,
+  offset: b
+});
+LR.CalculatePoints();
+LR.CalculateNoisedPoints();
 
-const interval = [-N + 10, N + 10]
-console.log(`y = ${k}x + ${b}`);
-const points: [number, number][] = [];
-for (let x = interval[0]; x <= interval[1]; x++) {
-  points.push([x, y(x)]);
-}
-console.log('Points:', points.map(p => `(${p[0]},${p[1]})`).join(', '));
-
-const noisedPoints: [number, number][] = [];
-const noiseStd = N / 5;
-for (let i = 0; i < points.length; i++) {
-  const [x, y] = points[i];
-  const noise = (Math.random() - 0.5) * noiseStd;
-  noisedPoints.push([x, y + noise]);
-}
-console.log('Points:', noisedPoints.map(p => `(${p[0]},${p[1]})`).join(', '));
-
-const linearRegression = (x: number[], y: number[]) => {
-  const n = x.length;
-  const sumX = x.reduce((a,b)=>a+b, 0);
-  const sumY = y.reduce((a,b)=>a+b, 0);
-  const sumXY = x.reduce((acc, val, i) => acc + val * y[i], 0);
-  const sumX2 = x.reduce((acc, val) => acc + val*val, 0);
-
-  const k = (n*sumXY - sumX*sumY) / (n*sumX2 - sumX*sumX);
-  const b = (sumY - k*sumX) / n;
-
-  return {k, b};
-}
-
-const reg = linearRegression(points.map(point => point[0]), noisedPoints.map(point => point[1]));
-console.log({reg})
+console.log({ eq: LR.equation, str: LR.equationString });
+console.log(LR.desmosCalculatedPoints);
+console.log(LR.desmosNoisedPoints);
+console.log(LR.CalculateLinearRegressionCoefficients());
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App noisedPoints={noisedPoints}/>
+    <App noisedPoints={LR.NoisedPoints}/>
   </StrictMode>,
 )
